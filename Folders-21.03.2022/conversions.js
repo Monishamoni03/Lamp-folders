520c86b7287fa0e2bcec5971e6cd9d2cd23589ad
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-03-21 17:21:16 +0530

    Add files via upload

---
+++ Folders-21.03.2022/conversions.js
@@ -0,0 +1,18 @@
+//string
+let value=true;
+alert(typeof value);
+value=String(value);
+alert(typeof value);
+
+//numeric
+alert("10"/"2");
+let str="15";
+alert(typeof str);
+let num=Number(str);
+alert(typeof num);
+
+//boolean
+alert(Boolean(1));
+alert(Boolean(0));
+alert(Boolean("Hello world"));
+alert(Boolean(""));
