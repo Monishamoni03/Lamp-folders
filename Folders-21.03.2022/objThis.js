6f52d201b9ab3fd0690cea98e2b12268f00dc620
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-03-21 23:38:31 +0530

    Add files via upload

---
+++ Folders-21.03.2022/objThis.js
@@ -0,0 +1,8 @@
+let user={
+    name:"sai",
+    branch:"EEE",
+    showMsg(){
+        alert(this.branch);
+    }
+};
+user.showMsg();
---
+++ Folders-21.03.2022/objects.js
@@ -0,0 +1,8 @@
+let user={
+    name:"sai",
+    branch:"EEE"
+};
+user.showMsg=function(){
+    alert("Hello World");
+};
+user.showMsg();
